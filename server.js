e042c71ed515f48e0a438bed5c5c4fb50d7bf9b5
MOOWOO <cdc.asic@gmail.com>
2022-01-09 15:53:25 +0900

    login handler from http header

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 require("dotenv").config();
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
-
-const server = new ApolloServer({
-  schema,
-});
+import { getUser } from "./users/users.utils";
 
 const PORT = process.env.PORT;
-
+const server = new ApolloServer({
+  schema,
+  context: async ({ req }) => {
+    return {
+      loggedInUser: await getUser(req.headers.token),
+    };
+  },
+});
 server
   .listen(PORT)
-  .then(() => console.log(`🚀 Server is running on http://localhost:${PORT}/`));
+  .then(() =>
+    console.log(`🚀Server is running on http://localhost:${PORT} ✅`)
+  );
