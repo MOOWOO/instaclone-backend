bbed947684878209d9dc002b9c6e6a15537f5a15
MOOWOO <cdc.asic@gmail.com>
2022-01-09 15:55:07 +0900

    user protection

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ import { ApolloServer } from "apollo-server";
 import schema from "./schema";
 import { getUser } from "./users/users.utils";
 
+require("dotenv").config();
+import { ApolloServer } from "apollo-server";
+import schema from "./schema";
+import { getUser, protectResolver } from "./users/users.utils";
+
 const PORT = process.env.PORT;
 const server = new ApolloServer({
   schema,
   context: async ({ req }) => {
     return {
       loggedInUser: await getUser(req.headers.token),
+      protectResolver,
     };
   },
 });
