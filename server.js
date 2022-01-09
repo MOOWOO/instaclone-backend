579e9053e427bad42e3c9932f5b87867a8552a88
MOOWOO <cdc.asic@gmail.com>
2022-01-09 16:17:18 +0900

    function oriented encapsule protectResolver

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
+ 
+   
 require("dotenv").config();
 import { ApolloServer } from "apollo-server";
 import schema from "./schema";
 import { getUser } from "./users/users.utils";
-
-require("dotenv").config();
-import { ApolloServer } from "apollo-server";
-import schema from "./schema";
-import { getUser, protectResolver } from "./users/users.utils";
 
 const PORT = process.env.PORT;
 const server = new ApolloServer({
@@ -14,7 +11,6 @@ const server = new ApolloServer({
   context: async ({ req }) => {
     return {
       loggedInUser: await getUser(req.headers.token),
-      protectResolver,
     };
   },
 });
