249b594eec2725d0b67a6204ca2648fe4f7fed2a
MOOWOO <cdc.asic@gmail.com>
2022-01-16 17:03:44 +0900

    migrate prisma

--- schema.js
+++ schema.js
@@ -1,4 +1,6 @@
-import { loadFilesSync, mergeResolvers, mergeTypeDefs } from "graphql-tools";
+const { loadFilesSync } = require("@graphql-tools/load-files");
+const { mergeTypeDefs, mergeResolvers } = require("@graphql-tools/merge");
+//const { makeExecutableSchema } = require("@graphql-tools/schema");
 
 const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
 const loadedResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.js`);
